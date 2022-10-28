5fab2a7de9e4a4a6ad9a03df0cb31304ae5a1d10
Dhanesh Kumar <dmeghwar.bscs18seecs@seecs.edu.pk>
2022-10-28 17:11:27 +0500

    finalized both sections

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,11 +2,11 @@ import react from 'react';
 
 function HeroSection() {
     return (
-        <div className='hero-section'>
-            <img src='images/gallery.png' alt='gallery photo'></img>
-            <h1>Online Experience</h1>
-            <p>Join Unique interactive activities led by <br></br>on-of-a-kind hosts-all without leaving<br></br> home.</p>
-        </div>
+        <section className='hero-section'>
+            <img className='gallery' src='images/gallery.png' alt='gallery photo'></img>
+            <h1 className='hero-header' >Online Experience</h1>
+            <p className='hero-para' >Join Unique interactive activities led by <br></br>on-of-a-kind hosts-all without leaving<br></br> home.</p>
+        </section>
     );
 }
 
