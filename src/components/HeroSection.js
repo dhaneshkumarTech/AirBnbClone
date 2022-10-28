03f643503600a805c815fd640814a13a5e1b4934
Dhanesh Kumar <dmeghwar.bscs18seecs@seecs.edu.pk>
2022-10-28 16:54:18 +0500

    nav bar and Hero section completed

---
+++ src/components/HeroSection.js
@@ -0,0 +1,13 @@
+import react from 'react';
+
+function HeroSection() {
+    return (
+        <div className='hero-section'>
+            <img src='images/gallery.png' alt='gallery photo'></img>
+            <h1>Online Experience</h1>
+            <p>Join Unique interactive activities led by <br></br>on-of-a-kind hosts-all without leaving<br></br> home.</p>
+        </div>
+    );
+}
+
+export default HeroSection;
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import react from 'react';
+
+function Navbar() {
+    return (
+        <div className='navbar-component'>
+            <img src='images/airbnb-logo.png' alt='AirBnb logo'></img>
+        </div>
+    );
+}
+
+export default Navbar;
