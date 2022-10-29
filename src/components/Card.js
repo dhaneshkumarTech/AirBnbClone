124020b6bf0f3ed7a93008bef6c2402a88276f35
Dhanesh Kumar <dmeghwar.bscs18seecs@seecs.edu.pk>
2022-10-29 15:48:44 +0500

    Card (Card Component: CSS of card component upadate for updating UX)

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,20 +2,19 @@ import React from 'react'
 
 function Card(props) {
     return (
-        <div className="card">
+        <a href='#' className="card">
             <img className='card-image' src={props.img} alt='Side view image'></img>
             <div className="card-info">
-                <span className='address'>{props.address}</span>
+                <span>{props.address}</span>
                 <div className='rating'>
                     <img src='images/star.png' className='star'></img>
                     <span className='rank'>{props.rating}</span>
                 </div>
-
             </div>
             <p className='place-distance'>{props.distance} Kilometer Away</p>
             <p className='date'>{props.date}</p>
             <p className='night-price'><span className='price'>${props.price}</span> night</p>
-        </div>
+        </a>
     )
 }
 
